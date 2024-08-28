3d5a2a9ae54e34fc6347fe2959a2d0bfdcaee165
liushilin <lsl200817@163.com>
2024-08-28 19:42:30 +0800

    2

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-set -e
 pnpm build
 cd dist/element-plus
 npm login --registry=https://packages.aliyun.com/6195ebe86f0b2d596ab944f3/npm/npm-registry/ --username=6001bc8b4fb2132a196ca8b5
